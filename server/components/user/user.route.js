8a86aa54673f26320dc49d4acb59e5898dd8c522
f0lg0 <folgonileonardo@gmail.com>
2020-07-17 20:41:43 +0200

    added auth route IMPORTANT: read comments

--- server/components/user/user.route.js
+++ server/components/user/user.route.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const login = require('./login.controller');
 const signup = require('./signup.controller');
+const auth = require('./isAuthorized');
 
 router.get('/login', login.getLogin);
 router.get('/signup', signup.getSignup);
+router.get('/auth', auth.isAuthorized);
 
 router.post('/signup', signup.signupUser);
 router.post('/login', login.loginUser);
