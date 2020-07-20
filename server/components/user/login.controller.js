a6b4dbc2dce8ea6dfc65b5a91dc65382e8f1f75e
JacopoFB <jacopofb03@gmail.com>
2020-07-19 22:43:53 -0600

    Added ability to use email or username to login
    Before you could only use your email to login, buit I changed it so you can use either. I also reformatted some of the js (we can change back if you dont like it) I also made another local middleware function that logins a valid user (you will see what I mean)

--- server/components/user/login.controller.js
+++ server/components/user/login.controller.js
@@ -2,53 +2,69 @@ const bcrypt = require('bcrypt');
 const Token = require('../tokens/tokenHandler');
 const User = require('./user.model');
 
-exports.getLogin = (req, res) => {
-    res.json({
-        message: 'DevsBunker Login Page',
-    });
-}
-
+// Local Middleware
 function failedLogin(res, next, msg) {
     res.status(422);
     const error = new Error(msg)
     next(error); // everything gets forwared to the error handler in server.js
 }
 
-exports.loginUser = async (req, res, next) => {
-    const toLook = await User.findOne({
-            email: req.body.email
-        })
-        .then(user => {
-            if (user) {
-                bcrypt.compare(req.body.password, user.password)
-                    .then((result) => {
-                        if (result) {
-                            // set the JWT token (payload = user id and username) 
-                            const loginToken = Token.createWebToken(user);
+/*
+    This function takes a request and user as a parameter (assumes that the user is a valid user in the database) and will check to make sure the password is correct    
+*/
+function loginValidUser(req, res, user){
+    bcrypt.compare(req.body.password, user.password).then((result) => {
+        if (result) {
+            // set the JWT token (payload = userByEmail id and username) 
+            const loginToken = Token.createWebToken(user);
 
-                            if (loginToken) {
-                                res.json({
-                                    message: 'Successfully logged in.',
-                                    token: loginToken
-                                });
-                            } else {
-                                failedLogin(res, next, 'Unable to create token.');
-                            }
-
-
-                        } else {
-                            failedLogin(res, next, 'Unable to login.');
-                        }
-
-                    })
-                    .catch(err => {
-                        next(err); // this is an internal error (bcrypt error, if it cant match the hash then it just returns undefined, if we get here something has gone wrong)
-                    })
+            if (loginToken) {
+                res.json({
+                    message: 'Successfully logged in.',
+                    token: loginToken
+                });
             } else {
-                failedLogin(res, next, 'Unable to login');
+                failedLogin(res, next, 'Unable to create token.');
             }
-        })
-        .catch(err => {
-            next(err); // db internal error
-        });
+
+
+        } else {
+            failedLogin(res, next, 'Unable to login.');
+        }
+    }).catch(err => {
+        console.log(err);
+        next(err); // this is an internal error (bcrypt error, if it cant match the hash then it just returns undefined, if we get here something has gone wrong)
+    })        
+}
+
+
+// Route Handlers
+exports.getLogin = (req, res) => {
+    res.json({
+        message: 'DevsBunker Login Page',
+    });
+}
+
+exports.loginUser = async (req, res, next) => {
+    const emailCheck = await User.findOne({
+        email: req.body.email
+    }).then(userByEmail => {
+        if (userByEmail) {
+            loginValidUser(req, res, userByEmail);
+        } else {
+            const usernameCheck = User.findOne({
+                username: req.body.username    
+            }).then(userByUsername => {
+                if(userByUsername){
+                    loginValidUser(req, res, userByUsername);        
+                }else{
+                    failedLogin(res, next, 'Your Username or Email is Incorrect');
+                }
+            }).catch(err => {
+                        next(err); // db internal error    
+            });
+        }
+    }).catch(err => {
+        next(err); // db internal error
+    });
 };
