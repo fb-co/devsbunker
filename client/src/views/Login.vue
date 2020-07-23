bf56641e73b07f87baae58c79ea264bcc3474d1e
JacopoFB <jacopofb03@gmail.com>
2020-07-23 10:42:09 -0600

    Basic Login page resizing

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,34 +1,25 @@
 <template>
     <div class="login">
-        <div class="content_container">
-            <div class="content_subContainer">
-
-                <NavBar headerText="Login"></NavBar>
+        <NavBar headerText=""></NavBar>
                 
-                <img src="https://wweb.dev/resources/navigation-generator/logo-placeholder.png" alt="Logo" class="large_logo" />
+        
+        <div id="error">{{ errMessage }}</div>
 
-                <div id="error">{{ errMessage }}</div>
+        <div class="loginForm">
+            <form name="login" v-on:submit.prevent="submitForm">
 
-                <div class="loginForm">
-                    <form name="login" v-on:submit.prevent="submitForm">
+                <label class="textLabel" for="userID">Username or Email</label>
+                <p class="required_symbol">*</p>
+                <input id="userID" name="userID" v-model="userID" autocapitalize="off" required>
 
-                        <label class="textLabel" for="userID">Username or Email</label>
-                        <p class="required_symbol">*</p>
-                        <input id="userID" name="userID" v-model="userID" autocapitalize="off" required>
+                <label class="textLabel" for="password">Password</label>
+                <p class="required_symbol">*</p>
+                <input type="password" id="password" name="password" v-model="password" required>
 
-                        <label class="textLabel" for="password">Password</label>
-                        <p class="required_symbol">*</p>
-                        <input type="password" id="password" name="password" v-model="password" required>
-
-                        <input type="submit" value="Login">
-                    </form>
-
-                </div>
-                <router-link class="bigRoute" to="/signup">Don't have an account? Create one here.</router-link>
-
-            </div>
+                <input type="submit" value="Login">
+            </form>
         </div>
-        <Footer class = "mainFooter"></Footer>
+        <!--<router-link class="bigRoute" to="/signup">Don't have an account? Create one here.</router-link>-->
     </div>
 </template>
 
@@ -38,7 +29,6 @@ import UserService from "../services/user.service";
 import SharedMethods from "../shared";
 
 import NavBar from "@/components/NavBar";
-import Footer from "@/components/Footer";
 
 export default {
     created() {
@@ -60,8 +50,7 @@ export default {
         };
     },
     components: {
-        NavBar,
-        Footer
+        NavBar
     },
     methods: {
         async submitForm() {
@@ -84,9 +73,11 @@ export default {
 
 .loginForm {
     padding: 20px;
-    width: 60%;
+    width: 500px;
     max-width: 800px;
     margin: auto;
+    background-color: blue;
+    border-radius: 10px;
 }
 
 input,
@@ -144,7 +135,17 @@ input[type="submit"]:hover {
     #error {
         width: 100%;
     }
+    .loginForm {
+        width: 60%;
+    }
 }
+@media only screen and (max-width: 450px) {
+    .loginForm {
+        display: none;
+    }
+}
+
+
 
 .bigRoute {
     color: var(--link-color);
