5b4b5f854d0e333f19ea9a0cab1e1569623e0b03
JacopoFB <jacopofb03@gmail.com>
2020-07-26 23:05:18 -0600

    Reverting burger menu
    After a few buckets of tears, I decided that it would be easier to just figure out why the menu was causing the page to scroll, so ill do this later but im reverting for now.

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,5 +1,11 @@
 <template>
     <nav class="menu-container">
+        <!-- burger menu -->
+        <input type="checkbox" aria-label="Toggle menu" />
+        <span></span>
+        <span></span>
+        <span></span>
+
         <!-- Header Text -->
         <div class="header_text">
             <div class="vertical_center horizontal_center">
@@ -25,12 +31,6 @@
                     <router-link to="/login" class="main_link">Login</router-link>
                 </li>
             </ul>
-            <!-- burger menu -->
-            <div id = "burger_menu">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
         </div>
     </nav>
 </template>
@@ -49,11 +49,9 @@ body {
     font-family: Sans-Serif;
     margin: 0;
 }
-
 .router-link-exact-active {
     background-color: var(--accent);
 }
-
 .menu-container {
     position: relative;
     display: flex;
@@ -67,27 +65,34 @@ body {
     user-select: none;
     box-sizing: border-box;
 }
-
 .menu-logo {
     line-height: 0;
     margin: 0 20px;
     z-index: 1;
 }
-
 .menu-logo img {
     max-height: 40px;
     max-width: 100px;
     flex-shrink: 0;
 }
-
 .menu-container a {
     text-decoration: none;
     color: var(--secondary-color);
     transition: color 0.3s ease;
 }
-
 .menu-container a:hover {
     color: #2c6975;
+}
+.menu-container input {
+    display: block;
+    width: 35px;
+    height: 25px;
+    margin: 0;
+    position: absolute;
+    cursor: pointer;
+    opacity: 0; /* hide this */
+    z-index: 2; /* and place it over the hamburger */
+    -webkit-touch-callout: none;
 }
 
 /* Burger menu */
@@ -97,12 +102,40 @@ body {
     height: 4px;
     margin-bottom: 5px;
     position: relative;
-    background: var(--main-font-color);
+    background: #f2f2f2;
     border-radius: 3px;
     z-index: 1;
     transform-origin: 4px 0px;
     transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1),
         background 0.5s cubic-bezier(0.77, 0.2, 0.05, 1), opacity 0.55s ease;
+}
+.menu-container span:first-child {
+    transform-origin: 0% 0%;
+}
+.menu-container span:nth-child(3) {
+    transform-origin: 0% 100%;
+}
+.menu-container input:checked ~ span {
+    opacity: 1;
+    transform: rotate(45deg) translate(3px, -1px);
+    background: var(--secondary-color);
+}
+.menu-container input:checked ~ span:nth-child(4) {
+    opacity: 0;
+    transform: rotate(0deg) scale(0.2, 0.2);
+}
+.menu-container input:checked ~ span:nth-child(3) {
+    transform: rotate(-45deg) translate(-5px, 11px);
+}
+.menu ul {
+    list-style: none;
+}
+.menu li {
+    font-size: 18px;
+    margin: -20px 0px -20px 0px;
+}
+.menu li:last-child {
+    margin: -20px -20px -20px 0px;
 }
 
 .main_link {
@@ -110,66 +143,67 @@ body {
     padding: 30px;
     box-sizing: border-box;
 }
-
 .header_text {
     margin: -20px;
     position: absolute;
     width: 100%;
     height: 100%;
 }
-
 /* mobile styles */
 @media only screen and (max-width: 767px) {
     .menu-container {
         flex-direction: column;
         align-items: flex-end;
     }
-
     .menu-logo {
         position: absolute;
         left: 0;
         top: 50%;
         transform: translateY(-50%);
     }
-
     .menu-logo img {
         max-height: 30px;
     }
-
-    /* Burger Menu */
-    #burger_menu {
-        position: relative;
-        top: 5px;
-        display: inline-block;
+    .menu {
+        position: absolute;
+        box-sizing: border-box;
+        width: 300px;
+        right: -300px;
+        top: 0;
+        margin: -20px;
+        padding: 75px 50px 50px;
+        background: var(--main-font-color);
+        -webkit-font-smoothing: antialiased;
+        /* to stop flickering of text in safari */
+        transform-origin: 0% 0%;
+        transform: translateX(0%);
+        transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1);
     }
-    #burger_menu:hover {
-        background-color: green;
+    .menu-container input:checked ~ .menu {
+        transform: translateX(-100%);
     }
-    .menu ul {
-        display: none;
+    /*
+    .menu-container input:not(:checked) ~ .menu {
+        
+        
     }
+    */
 }
-
 /* desktop styles */
 @media only screen and (min-width: 768px) {
     .menu-container {
         width: 100%;
     }
-
     .menu-container a {
         color: var(--main-font-color);
     }
-
     .menu-container input {
         display: none;
     }
-
-    /* Burger Menu */
-    #burger_menu {
+    /* Burger menu */
+    .menu-container span {
         display: none;
     }
-
-    /* Show the inline nav links */
     .menu {
         position: relative;
         width: 100%;
@@ -177,16 +211,8 @@ body {
         justify-content: space-between;
     }
     .menu ul {
-        list-style: none;
         padding: 0;
         display: flex;
-    }
-    .menu li {
-        font-size: 18px;
-        margin: -20px 0px -20px 0px;
-    }
-    .menu li:last-child {
-        margin: -20px -20px -20px 0px;
     }
 }
 </style>
