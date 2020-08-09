41390ca66dbb58d7a9fee1054e486ba99a830150
JacopoFB <jacopofb03@gmail.com>
2020-08-09 13:28:48 -0600

    Started implementing settings page mockup

--- client/src/components/SettingsList.vue
+++ client/src/components/SettingsList.vue
@@ -7,14 +7,17 @@
                     <input type="text" name="search" class="search-bar" placeholder="Search a setting...">
                     <li class="settings_list_item" v-for="(option, index) in Object.keys(settings)" :key="index">
                         <!-- todo: pull icons from a template like for the Settings -->
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user option-icon" width="23" height="23" viewBox="0 0 24 24" stroke-width="1.5" stroke="var(--main-font-color)" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" />
-                            <circle cx="12" cy="7" r="4" />
-                            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-                        </svg>
-                        {{ option }}
+                        <div class="option_icon_container">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user option-icon" width="23" height="23" viewBox="0 0 24 24" stroke-width="1.5" stroke="var(--main-font-color)" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" />
+                                <circle cx="12" cy="7" r="4" />
+                                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                            </svg>
+                        </div>
 
-                        <p class="option-arrow">></p>
+                        <p class="option_text">{{ option }}</p>
+
+                        <p class="option-arrow option_text">></p>
                     </li>
                 </ul>
             </div>
@@ -42,7 +45,7 @@ export default {
     padding-top: 20px;
     margin-right: auto;
     margin-left: auto;
-    width: 1000px;
+    width: 80%;
     display: flex;
     flex-direction: row;
 }
@@ -60,14 +63,16 @@ export default {
 }
 .settings_list_item {
     border-left: 2px solid transparent;
-    box-sizing: border-box;
     width: 250px;
-    padding: 15px;
-    text-align: left;
+    display: flex;
+    flex-direction: row;
+    margin-top: 15px;
+    margin-bottom: 15px;
 }
 .settings_list_item:hover {
     border-left: 2px solid var(--accent);
     cursor: pointer;
+    font-weight: bold;
 }
 
 .search-bar {
@@ -86,16 +91,32 @@ export default {
 .search-bar::placeholder {
     color: #bcbcbc;
 }
-
+.option_icon_container {
+    padding-top: 10px;
+    padding-bottom: 10px;
+}
 .option-icon {
-    margin-right: 15px;
+    margin-top: 3px;
+    margin-left: 15px;
+    margin-right: 15px;    
+}
+.option_text {
+    margin-top: 3px;
+    display: inline-block;
+    line-height: 47px;
+}
+.option-arrow {
+    flex-grow: 1;
+    text-align: right;
+    display: inline-block;
 }
 
-.option-arrow {
-    text-align: right;
-    width: 10px;
-    height: 20px;
 
-    display: inline-block;
+/* Media Queries */
+
+@media only screen and (max-width: 767px) {
+    .pageSubcontainer{
+        width: 100%;
+    }
 }
 </style>
