5e92f17218a9415885f16d594928480d3ed44ee6
f0lg0 <folgonileonardo@gmail.com>
2020-08-14 14:36:16 +0200

    new: global component theme switcher
    I'm still not sure if we need this to be global

--- client/src/components/global/GlobalComponents.js
+++ client/src/components/global/GlobalComponents.js
@@ -1,7 +1,9 @@
 import Dropdown from "./Dropdown.vue";
 import CustomInput from "@/components/CustomInput";
+import ThemeSwitcher from './ThemeSwitcher.vue';
 
 export default {
     Dropdown,
-    CustomInput
+    CustomInput,
+    ThemeSwitcher
 };
