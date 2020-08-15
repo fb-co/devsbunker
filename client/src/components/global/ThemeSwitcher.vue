4144010131e6b7c1ba5e655926f7ac4f5366b792
f0lg0 <folgonileonardo@gmail.com>
2020-08-15 10:35:30 +0200

    fix: theme switcher icon denotes the current theme

--- client/src/components/global/ThemeSwitcher.vue
+++ client/src/components/global/ThemeSwitcher.vue
@@ -1,11 +1,11 @@
 <template>
     <div v-on:click="switchTheme()" id="theme-switcher-container">
-        <svg v-if="darkTheme" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-sun theme-switcher-icon" :width="width" :height="height" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <svg v-if="!darkTheme" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-sun theme-switcher-icon" :width="width" :height="height" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" />
             <circle cx="12" cy="12" r="4" />
             <path d="M3 12h1M12 3v1M20 12h1M12 20v1M5.6 5.6l.7 .7M18.4 5.6l-.7 .7M17.7 17.7l.7 .7M6.3 17.7l-.7 .7" />
         </svg>
-        <svg v-if="!darkTheme" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-moon theme-switcher-icon" :width="width" :height="height" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <svg v-if="darkTheme" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-moon theme-switcher-icon" :width="width" :height="height" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" />
             <path d="M16.2 4a9.03 9.03 0 1 0 3.9 12a6.5 6.5 0 1 1 -3.9 -12" />
         </svg>
