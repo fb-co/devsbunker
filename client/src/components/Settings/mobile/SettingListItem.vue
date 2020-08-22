f4f5cadbff9e9d1c5394fcd34d558659960cc4d7
f0lg0 <folgonileonardo@gmail.com>
2020-08-22 21:38:01 +0200

    fix: mobile settings page
    I kept the same schema we used for the desktop section

---
+++ client/src/components/Settings/mobile/SettingListItem.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="settings_list_item no_select" @click="navigateTo()">
+        <p class="setting-label">{{ label }}</p>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["label"],
+};
+</script>
+
+<style scoped>
+.settings_list_item {
+    display: flex;
+    flex-direction: column;
+    margin-top: 15px;
+    margin-bottom: 15px;
+}
+
+.setting-label {
+    text-align: left;
+    color: var(--main-font-color);
+    font-size: 20px;
+    font-weight: bold;
+
+    padding-top: 20px;
+    padding-left: 30px;
+    padding-bottom: 20px;
+}
+</style>
