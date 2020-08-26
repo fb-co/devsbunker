8bff6f7b75f09a002014469166070796c76a3350
JacopoFB <jacopofb03@gmail.com>
2020-08-25 21:30:40 -0600

    Ran outta time
    Would have done more but ran out of time, basically did nothing

--- client/src/components/Settings/desktop/SettingSections/Account.vue
+++ client/src/components/Settings/desktop/SettingSections/Account.vue
@@ -2,10 +2,17 @@
     <WrapperDesktop>
         <div id='section'>
             <div class='setting_labels'>
-
+                <div class='setting_container'>
+                    <p class='setting_label'>Account Visibility</p>
+                </div>
             </div>
             <div class='setting_cont'>
-
+                <div class='setting_container'>
+                    <Dropdown spacing="5px" label="Temp Label">
+                        <li>Hello</li>
+                        <li>Bruh</li>
+                    </Dropdown>
+                </div>
             </div>
         </div>
     </WrapperDesktop>
