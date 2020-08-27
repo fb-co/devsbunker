3553be5e353cb1cfbab71763ba442e3b33f73b0c
f0lg0 <folgonileonardo@gmail.com>
2020-08-27 10:09:03 +0200

    edit: tweaked dropdown
    I don't know if you like it but in my opinion it is more elegant. I don't like that bold effect when you hover over stuff, I think it is better to have a softer text and when you hover over it it should become darker, so there isn't that increase in fot size due to the bold effect

--- client/src/components/global/Dropdown.vue
+++ client/src/components/global/Dropdown.vue
@@ -1,6 +1,6 @@
 <!-- As of now the content box cannot contain any p tags -->
 <template>
-    <div id="component_container" :style='cssProps'> 
+    <div id="component_container" :style='cssProps'>
         <div class="label_container">
             <p class="label">{{ label }}</p>
             <svg class="inline-icon-spacer relative_vertical_center" width="8" height="8" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -12,79 +12,82 @@
 
             </slot>
         </div>
-    </div>    
+    </div>
 </template>
 
 <script>
 export default {
     props: {
         label: String, // label to be shown
-        spacing: {     // amount of space between links
+        spacing: {
+            // amount of space between links
             type: String,
-            default: '10px'
-        }
+            default: "10px",
+        },
     },
     computed: {
         cssProps() {
             return {
-                '--link-spacing': this.spacing
-            }
-        }
-    }
-}
+                "--link-spacing": this.spacing,
+            };
+        },
+    },
+};
 </script>
 
 <style scoped>
-    .label_container {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        width: 100%;
-        height: 100%;
-        border-radius: 4px;
-    }
-    .label {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-    }
-    .content {
-        display: none;
-        box-shadow: 0px 4px 40px rgba(0, 0, 0, 0.5);
-        position: relative;
-        z-index: 1;
-    }
+.label_container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    border-radius: 4px;
+}
+.label {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+.content {
+    display: none;
+    background: var(--main-color);
+    box-shadow: 0px 4px 40px rgba(0, 0, 0, 0.15);
+    position: relative;
+    z-index: 1;
 
-    /* Functionality */
+    border-radius: 20px;
+}
 
-    .content:hover{
-        display: block;
-    }
-    .label_container:hover + .content {
-        display: block;
-    }
-    .label_container:hover {
-        font-weight: bold;
-    }
-    
+/* Functionality */
 
-    .content > * {
-        height: var(--link-spacing);
-        width: 100%;
-        border: none;
-        text-align: center;
-        font-size: 15px;
-        font-family: rubik;
-        background-color: transparent;
-        color: var(--main-font-color);
-    }
-    .content > *:hover {
-        font-weight: bold;
-        cursor: pointer;
-    }
+.content:hover {
+    display: block;
+}
+.label_container:hover + .content {
+    display: block;
+}
+.label_container:hover {
+    font-weight: bold;
+}
 
-    #component_container:hover {
-        font-weight: bold;
-        cursor: pointer;
-    }
+.content > * {
+    height: var(--link-spacing);
+    width: 100%;
+    border: none;
+    text-align: center;
+    font-size: 15px;
+    font-family: rubik;
+    background-color: transparent;
+    color: var(--soft-text);
+}
+.content > *:hover {
+    color: var(--main-font-color);
+    cursor: pointer;
+}
+
+#component_container:hover {
+    font-weight: bold;
+    cursor: pointer;
+}
 </style>
