aa177a46fdbebab59d3ff174882694c9464e0c17
JacopoFB <jacopofb03@gmail.com>
2020-09-21 20:25:23 -0600

    Started on working graphql endpoint for user (DESC)
    So it wont work yet. Im trying to get the result of an async function in the resolve block but I just can get it work. If I could get a second pair of eyes it would be nice. Iv tried returning the actual promise, and returning the value of the promise but nothing is working. I left a bunch of comments if they help at all

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -1,15 +1,42 @@
+import User from "../components/user/user.model.js"; // the db
+
 import graphql from "graphql";
 const { GraphQLSchema, GraphQLObjectType, GraphQLString } = graphql;
+
+
+// DB Communication Methods
+
+async function getUserEntry(name) {
+    const userEntry = await User.findOne({ username: name }).then((user) => {
+        if (user) {
+            //console.log(user); // Its finding the user entry just fine, but the promise in the resolver is not receiving it for some reason
+            return user;
+        }
+    }).catch((err) => {
+        console.log(err)
+    });
+    // Not sure how to handle the error since this function isnt really middleware
+}
+
+// Graphql Endpoints
 
 const PersonType = new GraphQLObjectType({
     name: "Person",
     description: "...",
 
     fields: () => ({
-        firstName: {
+        username: {
             type: GraphQLString,
-            resolve: (person) => person.firstName,
+            resolve: (person) => person.username,
         },
+        email: {
+            type: GraphQLString,
+            resolve: (person) => person.email,
+        },
+        tag: {
+            type: GraphQLString,
+            resolve: (person) => person.tag,
+        }
     }),
 });
 const QueryType = new GraphQLObjectType({
@@ -20,17 +47,34 @@ const QueryType = new GraphQLObjectType({
         person: {
             type: PersonType,
             args: {
-                id: { type: GraphQLString },
+                username: { type: GraphQLString },
             },
-            resolve: (root, args) => {
-                let person = { firstName: undefined };
-                if (args.id === "1") {
-                    person.firstName = "Jacopo";
-                } else if (args.id === "2") {
-                    person.firstName = "Leonardo";
-                }
+            resolve: (root, args) => {                
+                /*
+                let person = {
+                    username: args.username,
+                    email: undefined,
+                    tag: undefined
+                };
 
-                return person;
+                const userFromDb = getUserEntry(args.username).then((user) => {
+                    person.email = "PHUCC THIS";
+                    //person.email = user.email,
+                    //person.tag = user.tag;
+                    console.log(user);
+                }).catch((err) => {
+                    console.log(err);
+                });
+                */
+                
+                // WHY IS USER UNDEFINED ?!?!?!!?!?!?!??! (IV tried putting return statement inside then block)
+                // THIS IS GIVING ME CRIPPLING ANXIETY
+                const result = getUserEntry(args.username).then((user) => {
+                    console.log(result);  
+                }).catch((err) => {
+                    console.log(err);
+                });
+                return result;
             },
         },
     }),
