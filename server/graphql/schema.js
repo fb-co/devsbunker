06bc56e0f9a3b62e075487da6cce9d63af20a7fe
f0lg0 <folgonileonardo@gmail.com>
2020-09-26 13:52:23 +0200

    edit: renamed schema

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -3,52 +3,53 @@ import User from "../components/user/user.model.js"; // the db
 import graphql from "graphql";
 const { GraphQLSchema, GraphQLObjectType, GraphQLString } = graphql;
 
-
 // DB Communication Methods
 
 // gets user document from db with a username parameter, we could also make another for the id if you want
 async function getUserEntry(name) {
-    return new Promise(resolve => {
-        User.findOne({ username: name }).then((user) => {
-            resolve(user)
-        }).catch((err) => {
-            console.log(err)
-        });
-    });    
+    return new Promise((resolve) => {
+        User.findOne({ username: name })
+            .then((user) => {
+                resolve(user);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    });
 }
 
 // Graphql Endpoints
 
-const PersonType = new GraphQLObjectType({
-    name: "Person",
-    description: "...",
+const UserType = new GraphQLObjectType({
+    name: "User",
+    description: "User Type",
 
     fields: () => ({
         username: {
             type: GraphQLString,
-            resolve: (person) => person.username,
+            resolve: (user) => user.username,
         },
         email: {
             type: GraphQLString,
-            resolve: (person) => person.email,
+            resolve: (user) => user.email,
         },
         tag: {
             type: GraphQLString,
-            resolve: (person) => person.tag,
-        }
+            resolve: (user) => user.tag,
+        },
     }),
 });
 const QueryType = new GraphQLObjectType({
     name: "Query",
-    description: "...",
+    description: "Root Query",
 
     fields: () => ({
-        person: {
-            type: PersonType,
+        user: {
+            type: UserType,
             args: {
                 username: { type: GraphQLString },
             },
-            resolve: (root, args) => {                
+            resolve: (root, args) => {
                 /* I think its because before I was returning the result of the promise in 'getUserEntry()' while now I literally return a promise and just call the promise */
 
                 return getUserEntry(args.username);
