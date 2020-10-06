95f80aadfcc066cadb34be8c4daf722fde19c869
f0lg0 <folgonileonardo@gmail.com>
2020-10-06 21:03:53 +0200

    removed: REST endpoints for signup and login

--- server/components/user/user.route.js
+++ server/components/user/user.route.js
@@ -1,8 +1,6 @@
 import express from "express";
 const router = express.Router();
 
-import { getLogin, loginUser } from "./user.login.js";
-import { getSignup, signupUser } from "./user.signup.js";
 import logoutUser from "./user.logout.js";
 import isAuthorized from "./isAuthorized.js";
 import {
@@ -10,14 +8,10 @@ import {
     revokeUserSession,
 } from "../tokens/tokens.controller.js";
 
-router.get("/login", getLogin);
-router.get("/signup", getSignup);
 router.get("/auth", isAuthorized);
 
 router.post("/refresh_token", getAccessToken);
 router.post("/revoke_session", revokeUserSession);
-router.post("/signup", signupUser);
-router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 
 export default router;
