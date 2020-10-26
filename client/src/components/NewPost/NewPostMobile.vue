72b4aff001f32861877a69a357c847595798a2d3
Jacopo Folgoni Borsa <jacopofb03@gmail.com>
2020-10-26 08:54:44 -0600

    Added frontend for adding images

--- client/src/components/NewPost/NewPostMobile.vue
+++ client/src/components/NewPost/NewPostMobile.vue
@@ -13,6 +13,17 @@
         <GeneralInput class="field" label="Post Description" :isTextArea="true" />
 
         <div class="tag_container">
+            <p>Images (0/5)</p>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-camera-plus add_image" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <circle cx="12" cy="13" r="3" />
+                <path d="M5 7h2a2 2 0 0 0 2 -2a1 1 0 0 1 1 -1h2m9 7v7a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2" />
+                <line x1="15" y1="6" x2="21" y2="6" />
+                <line x1="18" y1="3" x2="18" y2="9" />
+            </svg>
+        </div>
+
+        <div class="tag_container">
             <p>Add Collaborators</p>
             <div class="add_tags">
                 <CreateTag label="Jacopo" />
@@ -61,6 +72,14 @@ import GeneralInput from "../global/GeneralInput.vue";
 import CreateTag from "./CreateTag.vue";
 
 export default {
+    data() {
+        return {
+            images: [],
+            contributers: [],
+            tags: [],
+            links: []
+        }
+    },
     methods: {
         close() {
             this.$parent.close();
@@ -166,4 +185,8 @@ export default {
         cursor: pointer;
     }
 
+    .add_image:hover {
+        stroke-width: 2px;
+        cursor: pointer;
+    }
 </style>
