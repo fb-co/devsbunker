8a5e796ef574ebcb686e17b1b8d0928cd2025144
f0lg0 <folgonileonardo@gmail.com>
2020-11-07 18:07:45 +0100

    fix: correctly displaying like amount

--- client/src/components/DesktopProjectCard.vue
+++ client/src/components/DesktopProjectCard.vue
@@ -26,7 +26,7 @@
                             d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"
                         />
                     </svg>
-                    
+
                     <!-- Filled icon -->
                     <svg
                         @mouseleave="likeIsActive = false"
@@ -45,13 +45,21 @@
                     </svg>
                 </div>
                 <div class="vertical_flex_center likeAmt">
-                    <p>{{ projectData.likeAmt }}  -</p>
+                    <p>{{ projectData.likeAmt }} -</p>
                 </div>
                 <div class="vertical_flex_center">
                     <p class="language">{{ projectData.bunkerTag }}</p>
                 </div>
-                <div class="spacer" v-if="projectData.price!=0 && projectData.price!=null">-</div>
-                <p class="price_text" v-if="projectData.price!=0 && projectData.price!=null">
+                <div
+                    class="spacer"
+                    v-if="projectData.price != 0 && projectData.price != null"
+                >
+                    -
+                </div>
+                <p
+                    class="price_text"
+                    v-if="projectData.price != 0 && projectData.price != null"
+                >
                     ${{ projectData.price }}
                 </p>
             </div>
@@ -83,12 +91,13 @@ export default {
     },
     methods: {
         likePost() {
-            GraphQLService.likePost(this.$store.getters.accessToken, this.projectData.id)
-                .then((newAmt) => {
-                    this.projectData.likeAmt = newAmt;
-                    console.log(newAmt);
-                })
-        }
+            GraphQLService.likePost(
+                this.$store.getters.accessToken,
+                this.projectData.id
+            ).then((res) => {
+                this.projectData.likeAmt = res.data.likePost.likeAmt;
+            });
+        },
     },
 };
 </script>
