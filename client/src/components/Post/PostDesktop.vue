242e39eb6df0a2907820ad41cb8e8f62308fe88d
f0lg0 <folgonileonardo@gmail.com>
2020-12-15 20:59:40 +0100

    new: progress on the desktop view post page

--- client/src/components/Post/PostDesktop.vue
+++ client/src/components/Post/PostDesktop.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
-        <LeftContent />
-        <!-- at the moment im passing an empty object, we should avoid re-fetching the notification object tho (maybe we can put it in the store) -->
-        <RightContent :notifications="[]" />
+        <div class="main_container">
+            <LeftContent />
+            <div class="center_content">{{ projectData }}</div>
+            <!-- at the moment im passing an empty array, we should avoid re-fetching the notification object tho (maybe we can put it in the store) -->
+            <RightContent :notifications="[]" />
+        </div>
     </div>
 </template>
 
@@ -22,4 +25,20 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.main_container {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+}
+
+.center_content {
+    -ms-overflow-style: none; /* IE and Edge (scrollbar hide)*/
+    scrollbar-width: none; /* Firefox (scrollbar hide)*/
+
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    background-color: red;
+}
+</style>
