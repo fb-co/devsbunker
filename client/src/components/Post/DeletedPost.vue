5543181475668e32467bd6b780c1d17dfa6864a2
f0lg0 <folgonileonardo@gmail.com>
2021-01-07 21:58:11 +0100

    new: deleted post component
    yea it's empty like me

---
+++ client/src/components/Post/DeletedPost.vue
@@ -0,0 +1,11 @@
+<!-- This is going to be the deleted post card thingy -->
+<template>
+    <div id="deleted"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
