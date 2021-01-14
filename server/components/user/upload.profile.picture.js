04688a09fb1f5465943f3302c24ba0a3ed0ff4f6
f0lg0 <folgonileonardo@gmail.com>
2021-01-14 20:56:14 +0100

    new: saving profile pic to user doc

--- server/components/user/upload.profile.picture.js
+++ server/components/user/upload.profile.picture.js
@@ -1,7 +1,8 @@
+import { UserInputError } from "apollo-server-express";
 import FilesHandler from "../../middlewares/FilesHandler.js";
 const filesHandler = new FilesHandler();
 
-// import User from "../user/user.model.js";
+import User from "../user/user.model.js";
 
 export default async function uploadProfilePicture(req, res, next) {
     try {
@@ -30,6 +31,13 @@ export default async function uploadProfilePicture(req, res, next) {
                 }
 
                 if (valid) {
+                    const userDoc = await User.findOne({
+                        _id: user._id,
+                    });
+
+                    userDoc.profile_pic = file.filename;
+                    await userDoc.save();
+
                     res.json({
                         message: "Successfully uploaded files.",
                         data: data,
@@ -46,6 +54,8 @@ export default async function uploadProfilePicture(req, res, next) {
                 throw new Error("Please upload a file.");
             }
         } else {
+            res.status(401);
+            throw new Error("Unauthorized");
         }
     } catch (err) {
         next(err);
