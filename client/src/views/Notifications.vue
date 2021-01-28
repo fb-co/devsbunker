fd95b89722ec180a12683fba93afb583b09fd558
JacopoFB <jacopofb03@gmail.com>
2021-01-28 16:26:24 -0700

    Started working on notifications page (had to go)

--- client/src/views/Notifications.vue
+++ client/src/views/Notifications.vue
@@ -9,10 +9,19 @@
 // The notification target key, is what the notification is refering to, for example, the post title that was liked, or the username of the person being followed
 
 import NavBar from "@/components/NavBar";
+//import GraphQLService from "@/services/graphql.service.js";
 
 export default {
+    data() {
+        return {
+            notifications: undefined
+        }
+    },
     components: {
         NavBar
+    },
+    created: {
+        //GraphQLService.getUnreadNotifications        
     }
 }
 </script>
