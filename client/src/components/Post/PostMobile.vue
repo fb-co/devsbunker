5667c2173e8eb694bbcf08491e8b9f28abd6bb6b
JacopoFB <jacopofb03@gmail.com>
2021-03-01 22:20:47 -0700

    Added a panel to mobile posts to see better

--- client/src/components/Post/PostMobile.vue
+++ client/src/components/Post/PostMobile.vue
@@ -2,69 +2,75 @@
     <div class="mobile_post_container">
         <div class="custom_header_container" ref="image_div">
             <div class="header_actions">
-                <svg
-                    @click="back()"
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="icon icon-tabler icon-tabler-arrow-left"
-                    width="30"
-                    height="30"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.2"
-                    stroke="var(--main-font-color)"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <line x1="5" y1="12" x2="19" y2="12" />
-                    <line x1="5" y1="12" x2="11" y2="18" />
-                    <line x1="5" y1="12" x2="11" y2="6" />
-                </svg>
-                <div style="flex-grow: 1"></div>
-                <!-- Placeholder -->
-                <div class="save_post_container">
+                <div class="back_arrow_container">
                     <svg
-                        @click="savePost(projectData.id)"
-                        v-if="!projectData.isSaved"
-                        width="25"
-                        height="25"
-                        stroke-width="0.7"
-                        stroke="var(--ssoft-text)"
-                        viewBox="0 0 16 16"
-                        class="bi bi-bookmark save_btn"
-                        fill="currentColor"
+                        @click="back()"
                         xmlns="http://www.w3.org/2000/svg"
+                        class="icon icon-tabler icon-tabler-arrow-left"
+                        width="30"
+                        height="30"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.2"
+                        stroke="var(--main-font-color)"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
                     >
-                        <path
-                            fill-rule="evenodd"
-                            d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"
-                        />
-                    </svg>
-                    <svg
-                        @click="unsavePost(projectData.id)"
-                        v-else
-                        width="25"
-                        height="25"
-                        stroke-width="0.7"
-                        stroke="var(--ssoft-text)"
-                        viewBox="0 0 16 16"
-                        class="bi bi-bookmark-fill save_btn"
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path fill-rule="evenodd" d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5V2z" />
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <line x1="5" y1="12" x2="19" y2="12" />
+                        <line x1="5" y1="12" x2="11" y2="18" />
+                        <line x1="5" y1="12" x2="11" y2="6" />
                     </svg>
                 </div>
+                
+                <!-- Placeholder -->
+                <div style="flex-grow: 1"></div>
+                
+                <div class="actions_container">
+                    <div class="save_post_container">
+                        <svg
+                            @click="savePost(projectData.id)"
+                            v-if="!projectData.isSaved"
+                            width="25"
+                            height="25"
+                            stroke-width="0.7"
+                            stroke="var(--ssoft-text)"
+                            viewBox="0 0 16 16"
+                            class="bi bi-bookmark save_btn"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"
+                            />
+                        </svg>
+                        <svg
+                            @click="unsavePost(projectData.id)"
+                            v-else
+                            width="25"
+                            height="25"
+                            stroke-width="0.7"
+                            stroke="var(--ssoft-text)"
+                            viewBox="0 0 16 16"
+                            class="bi bi-bookmark-fill save_btn"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path fill-rule="evenodd" d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5V2z" />
+                        </svg>
+                    </div>
 
-                <svg v-if="!projectData.isLiked" @click="likePost(projectData.id)" width="30" height="30" viewBox="0 0 16 16" class="bi bi-heart" fill="#eb4034" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        fill-rule="evenodd"
-                        d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"
-                    />
-                </svg>
-                <svg v-else @click="unlikePost(projectData.id)" width="30" height="30" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="#eb4034" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
-                </svg>
+                    <svg v-if="!projectData.isLiked" @click="likePost(projectData.id)" width="30" height="30" viewBox="0 0 16 16" class="bi bi-heart" fill="#eb4034" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            fill-rule="evenodd"
+                            d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"
+                        />
+                    </svg>
+                    <svg v-else @click="unlikePost(projectData.id)" width="30" height="30" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="#eb4034" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+                    </svg>
+                </div>
             </div>
             <div class="placeholder">
                 
@@ -135,6 +141,28 @@ export default {
     width: 100%;
 }
 
+.back_arrow_container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    background-color: var(--secondary-color);
+    box-shadow: 0px 4px 40px rgba(0, 0, 0, 0.5);
+}
+.back_arrow_container > svg {
+    cursor: pointer;
+    margin: 0 auto;
+}
+.actions_container {
+    display: flex;
+    flex-direction: row;
+    padding: 15px;
+    border-radius: 10px;
+    background-color: var(--secondary-color);
+    box-shadow: 0px 4px 40px rgba(0, 0, 0, 0.5);
+}
 .custom_header_container {
     display: flex;
     flex-direction: column;
