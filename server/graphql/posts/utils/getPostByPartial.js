beb4a126295f6267a7beda46aa9b217cd5d89008
JacopoFB <jacopofb03@gmail.com>
2021-04-12 21:49:31 -0600

    Started working on revamped query thats much more readable

--- server/graphql/posts/utils/getPostByPartial.js
+++ server/graphql/posts/utils/getPostByPartial.js
@@ -21,9 +21,28 @@ export default async function getPostByPartial(partial_name, filter, userToFilte
     // notes for when I pick this up: you can't run conditional queries in and or or chains because they will always evaluate to false (just dont use them)
     return new Promise((resolve) => {
         const regex = new RegExp(partial_name, "i");
+        
+        let postQuery = {};
+
+        // craft the query using all the variables and data
+        if (filter === "saved") {
+            
+        } else if (filter === "myProjects") {
+
+        }
+        
+        
+        
+        /* Old poo poo query (leaving for ref) 
         const lastPostIdQuery = lastPostId!=0 ? { _id: { $lt: lastPostId } } : {};
         //const lastUniqueFieldQuery = lastUniqueField!=-1 ? { likeAmt: { $lt: lastUniqueField } } : {};
-        let postQuery = {}; // post query for the filter, like filtering out for a certain title or name
+        let postQuery = {
+            $and: [
+                { _id: { $in: userPosts } },
+                { title: regex },
+                lastPostIdQuery,
+            ]
+        }; // post query for the filter, like filtering out for a certain title or name
         
         try {
             // get a query for the post filter
@@ -43,13 +62,7 @@ export default async function getPostByPartial(partial_name, filter, userToFilte
             // get posts given a sorting type
             if (sortingType === "Newest") {
                 console.log(lastPostId);
-                Posts.find({
-                    $and: [
-                        postQuery,
-                        { title: regex },
-                        lastPostIdQuery,
-                    ]
-                })
+                Posts.find(postQuery)
                 .sort({ _id: -1 })
                 .limit(loadAmt + 1)
                 .then((posts) => {
@@ -113,136 +126,6 @@ export default async function getPostByPartial(partial_name, filter, userToFilte
             }
         } catch (err) {
             throw new Error("There was a problem: " + err);
-        }
-        
-        /* Old stinky poo poo query (leaving for ref)
-        if (filter === "none") {
-            Posts.find({ title: regex })
-            .limit(loadAmt+1)
-            .then((posts) => { 
-                if (user) {
-                    const finalPosts = AddDynamicData.addAll(posts, user);
-                    resolve(finalPosts);
-                }
-                resolve(posts);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-        } else {
-            // search based on some filter
-            try {
-                let postQuery;
-                if (lastPostId != -1) {
-                    if (filter === "saved") {
-                        // need to find the posts objects since they are not contained in the user db document
-                        let userPosts = user.saved_posts;
-
-                        // for whatever reason to find by id, they need to wrapped in an ObjectType wrapper
-                        for (let i = 0; i < userPosts.length; i++) {
-                            userPosts[i] = new mongoose.Types.ObjectId(userPosts[i]);
-                        } 
-                        postQuery = { _id: { $in: userPosts } };
-                    } else if (filter === "myProjects") {
-                        postQuery = { author: userToFilter };
-                    }
-                    if (sortingType == "Newest") {
-                        // NEWEST POST SORT LOGIC
-                        if (lastPostId != 0) {
-                            Posts.find({
-                                $and: [
-                                    postQuery,
-                                    { _id: { $lt: lastPostId } },
-                                    { title: regex }
-                                ]
-                            })
-                            .limit(loadAmt+1)
-                            .sort({ _id: -1 })
-                            .then((results) => {
-                                if (user) {
-                                    const finalPosts = AddDynamicData.addAll(results, user);
-                                    resolve(finalPosts);
-                                }
-                                resolve(results);
-                            })
-                            .catch((err) => {
-                                console.log(err);
-                            });
-                        } else {
-                            Posts.find({
-                                $and: [
-                                    postQuery,
-                                    { title: regex }
-                                ]
-                            })
-                            .sort({ _id: -1 })
-                            .limit(loadAmt+1)
-                            .then((results) => {
-                                if (user) {
-                                    const finalPosts = AddDynamicData.addAll(results, user);
-                                    resolve(finalPosts);
-                                }
-                                resolve(results);
-                            })
-                            .catch((err) => {
-                                console.log(err);
-                            });
-                        }
-                    } else if (sortingType == "Most Popular") {
-                        // MOST POPULAR SORT LOGIC
-                        if (lastPostId != 0 && lastUniqueField != -1) {
-                            Posts.find({
-                                $or: [
-                                    {
-                                        postQuery,
-                                        likeAmt: { $lt: lastUniqueField }
-                                    },
-                                    {
-                                        $and: [
-                                            postQuery,
-                                            { _id: { $lt: lastPostId } },
-                                            { title: regex }
-                                        ],
-                                        likeAmt: { $lt: lastUniqueField }
-                                    }
-                                ]
-                            })
-                            .sort({ likeAmt: -1, _id: -1 })
-                            .limit(loadAmt+1)
-                            .then((results) => {
-                                if (user) {
-                                    const finalPosts = AddDynamicData.addAll(results, user);
-                                    resolve(finalPosts);
-                                }
-                                resolve(results);
-                            })
-                            .catch((err) => {
-                                console.log(err);
-                            });
-                        }
-                    }
-                } else {
-                    Posts.find({
-                        $and: [
-                            postQuery,
-                            { title: regex }
-                        ]
-                    })
-                    .limit(loadAmt+1)
-                    .then((results) => {
-                        if (user) {
-                            const finalPosts = AddDynamicData.addAll(results, user);
-                            resolve(finalPosts);
-                        }
-                        resolve(results);
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    });
-                }
-            } catch (err) {
-                throw new Error("There was a problem: " + err);
-            }
         }
         */
     });
