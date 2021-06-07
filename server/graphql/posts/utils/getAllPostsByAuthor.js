847f3bf2e10eb92437a46f50a9e07c05f8a296f0
f0lg0 <folgonileonardo@gmail.com>
2021-06-07 21:01:55 +0200

    new: checking for enabled field in getUserPost

--- server/graphql/posts/utils/getAllPostsByAuthor.js
+++ server/graphql/posts/utils/getAllPostsByAuthor.js
@@ -2,7 +2,7 @@ import Posts from "../../../components/post/post.model.js";
 
 export default function getAllPostsByAuthor(postAuthor) {
     return new Promise((resolve, reject) => {
-        Posts.find({ author: postAuthor })
+        Posts.find({ author: postAuthor, enabled: true })
             .then((posts) => {
                 if (posts) {
                     resolve(posts);
--- server/graphql/posts/utils/getPostById.js
+++ server/graphql/posts/utils/getPostById.js
@@ -2,13 +2,18 @@ import Posts from "../../../components/post/post.model.js";
 import User from "../../../components/user/user.model.js";
 
 export default async function getUserPost(postId) {
-    return new Promise((resolve) => {
-        Posts.findOne({ _id: postId })
+    return new Promise((resolve, reject) => {
+        Posts.findOne({ _id: postId, enabled: true })
             .then((post) => {
-                resolve(post);
+                if (posts) {
+                    resolve(post);
+                } else {
+                    reject(new Error("Unabled to find post"));
+                }
             })
             .catch((err) => {
                 console.log(err);
+                reject(err);
             });
     });
 }
