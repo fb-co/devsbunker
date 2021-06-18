d8e74115ab6fa3e59250c562a0f56d1353f38b33
f0lg0 <folgonileonardo@gmail.com>
2021-06-18 09:19:31 +0200

    edit: replaced axios with node-fetch because I dont want to learn it

--- server/components/github/oauth.controllers.js
+++ server/components/github/oauth.controllers.js
@@ -1,20 +1,21 @@
-import axios from "axios";
+import fetch from "node-fetch";
 
 export async function getToken({ query: { code } }, res) {
-    const body = {
+    const data = {
         client_id: process.env.OAUTH_CLIENT_ID,
         client_secret: process.env.OAUTH_CLIENT_SECRET,
         code,
     };
-    const opts = { headers: { accept: "application/json" } };
-    axios
-        .post("https://github.com/login/oauth/access_token", body, opts)
-        .then((_res) => _res.data.access_token)
-        .then((token) => {
-            console.log("token:", token);
-            // res.redirect(`http://192.168.1.10:8080/?token=${token}`);
 
-            res.send({ token });
-        })
-        .catch((err) => res.status(500).json({ err: err.message }));
+    const result = await fetch("https://github.com/login/oauth/access_token", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            accept: "application/json",
+            "Content-Type": "application/json",
+        },
+    });
+
+    const json = await result.json();
+    res.send({ json });
 }
