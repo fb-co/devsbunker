0537ec4d1ad685dad4062be8ebdd19a30c1589fa
JacopoFB <jacopofb03@gmail.com>
2021-07-23 22:27:10 -0600

    Added server side tag validation (desc)
    This means that the tag json file needs to be included in both the server and client server, so make sure to update it in BOTH if you add or remove tags

--- server/components/languages/languages.list.js
+++ server/components/languages/languages.list.js
@@ -1,14 +1,15 @@
-import LanguagesList from "../languages/LanguagesData.json";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const LanguageData = require("../languages/LanguagesData.json");
 
 const Languages = {
     list: LanguageData,
     isValid: function(tag) {
-        this.list.forEach(tag => {
-            if (tag.name === tag) {
+        for (let i = 0; i < this.list.length; i++) {
+            if (this.list[i].name === tag) {
                 return true;
             }
-        });
-        return false;
+        }
     }
 };
 
