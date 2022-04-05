2ce18e0f7e7a8774ab498204d823f807cdc7a3ad
f0lg0 <folgonileonardo@gmail.com>
2022-04-05 19:45:44 +0200

    fix: grabbing email address of github users even if they set it to be hidden

--- server/components/github/oauth.routes.js
+++ server/components/github/oauth.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 import { authorize } from "./oauth.controllers.js";
 
 router.get("/", (_, res) => {
-    res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.OAUTH_CLIENT_ID}`);
+    res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.OAUTH_CLIENT_ID}&scope=user:email`);
 });
 
 router.get("/get", authorize);
