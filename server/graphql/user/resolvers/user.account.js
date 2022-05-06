d7ee708625f7ee483184769d8a79b7c63c5f797b
Leonardo Folgoni <folgonileonardo@gmail.com>
2022-05-06 22:17:14 +0200

    fix: nuke #3

--- server/graphql/user/resolvers/user.account.js
+++ server/graphql/user/resolvers/user.account.js
@@ -947,10 +947,10 @@ export default {
                             html: `
                                 <p>To delete your account, follow this link: </p> 
                                 <a
-                                    href="${process.env.PROTOCOL}://${process.env.FRONTEND}:${process.env.CLIENTSIDE_PORT}/user/delete/${verification.userId}/${verification.token}"
+                                    href="${process.env.PROTOCOL}://${process.env.FRONTEND}/user/delete/${verification.userId}/${verification.token}"
                                     style="color: #067df7; text-decoration: none"
                                     target="_blank"
-                                    >${process.env.PROTOCOL}://${process.env.FRONTEND}:${process.env.CLIENTSIDE_PORT}/user/delete/${verification.userId}/${verification.token}</a
+                                    >${process.env.PROTOCOL}://${process.env.FRONTEND}/user/delete/${verification.userId}/${verification.token}</a
                                 >
                             `,
                         };
