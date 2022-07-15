e7a7d8f1c5a25411eac9520410812cf77bf59f3d
JacopoFB <jacopofb03@gmail.com>
2022-07-15 15:29:26 -0600

    Added views to post documents

--- client/src/views/Post.vue
+++ client/src/views/Post.vue
@@ -57,6 +57,7 @@ export default {
                 "links",
                 "collaborators",
                 "tags",
+                "views",
                 "createdAt",
                 `comments {
                     id
